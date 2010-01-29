7b73bb875e1824b0ca4f3a9d5ece93445a7cdaf9
tero.heino@gmail.com <tero.heino@gmail.com@a5b37900-4481-11de-b77d-cbd14124f061>
2010-01-29 19:38:46 +0000

    SMS/Twitter/configfile support
    git-svn-id: http://torstinator.googlecode.com/svn/trunk@12 a5b37900-4481-11de-b77d-cbd14124f061

--- Torstinator.py
+++ Torstinator.py
@@ -1,23 +1,44 @@
 #!/usr/bin/python2.5
 # -*- coding: utf-8 -*-
 
-# http://people.csail.mit.edu/hubert/pyaudio/
-import pyaudio
-# http://docs.python.org/library/logging.html
-import logging
 
 import audioop
-import optparse
 import time
-import sqlite3
+import ConfigParser
 import os
 import urllib2, cookielib, re
 from time import strftime
+import sys
 
-version = 0.7
 
-logging.basicConfig(level=logging.DEBUG,
+try:
+	import pyaudio 
+except ImportError:
+	print "You need to have pyaudio library (http://people.csail.mit.edu/hubert/pyaudio/)"
+	sys.exit(1)
+# http://docs.python.org/library/logging.html
+import logging
+
+sqlite_enabled = False
+
+try:
+	import sqlite3
+	sqlite_enabled = True
+except ImportError:
+	pass
+
+twitter_enabled = False
+try:
+	import twitter
+	twitter_enabled = True
+except ImportError:
+	pass
+
+version = 0.8
+
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s\t%(levelname)s\t%(message)s')
+
 
 
 def sendSms(username,password,sender,recipients,sms):
@@ -27,26 +48,19 @@ def sendSms(username,password,sender,recipients,sms):
 	##############################################################################
 	# This script will contact oma.saunalahti.fi and provides the sms service
 	##############################################################################
-	#    SaunaSMS is a small quick and dirty script which will provide a free
-	#    sms service for Saunalahti (http://www.saunalahti.fi) customers.
+	#    SaunaSMS is a small quick and dirty script which will provide a free sms service for Saunalahti (http://www.saunalahti.fi) customers.
 	#
-	#    Copyright (C) 2008  Juhapekka Piiroinen
+	#    Copyright (C) 2008 Juhapekka Piiroinen
 	#
-	#    This program is free software: you can redistribute it and/or modify
-	#    it under the terms of the GNU General Public License as published by
-	#    the Free Software Foundation, either version 3 of the License, or
-	#    (at your option) any later version.
+	#    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either 
+	#    version 3 of the License, or (at your option) any later version.
 	#
-	#    This program is distributed in the hope that it will be useful,
-	#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-	#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	#    GNU General Public License for more details.
+	#    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+	#    See the GNU General Public License for more details.
 	#
-	#    You should have received a copy of the GNU General Public License
-	#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	#    You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	#############################################################################
-	# Contact: juhapekka.piiroinen@gmail.com
-	# Version: 0.3
+	# Contact: juhapekka.piiroinen@gmail.com Version: 0.3
 	#############################################################################
 	multiple = 0
 	msgs = []
@@ -77,32 +91,88 @@ def sendSms(username,password,sender,recipients,sms):
 		s.close()
 
 class Torstinator:
-	debug = 0
-	limit = 0
 	con = None
 	day = None
-	username = None
-	password = None
 	sender = None
 	to = None
+	config = None
+	log_limit = None
+	use_sqlite = False
+	use_screenlog = True
+	use_twitter = False
+	twitter_limit = None
+	twitter_username = ""
+	twitter_password = ""
+	use_sms = False
+	sms_limit = None
+	sms_username = ""
+	sms_password = ""
+	sms_from = ""
+	sms_to = ""
+
+	twitapi = None
 
 	def __init__(self):
-		logging.info("Torstinator %.2f, Debug: %s, Limit: %d" % (version, self.debug, self.limit))
 		logging.debug("Initializing")
 		database_name = "noise_archive/noise.%s.db" % strftime("%Y-%m-%d")
 		self.day = strftime("%d")
-		logging.info("Connecting to database")
-		if not os.path.isfile(database_name):
-			logging.info("Database file not found")
-			self.con = sqlite3.connect(database_name,isolation_level=None)
-			c = self.con.cursor()
-			c.execute('''create table noise (datetime text, noise real);''')
-		else:
-			logging.info("Database file found")
-			self.con = sqlite3.connect(database_name,isolation_level=None)
-			logging.info("Database connected")
+		self.config = ConfigParser.ConfigParser()
+		self.config.readfp(open('Torstinator.cfg'))
+		
+		self.log_limit = self.config.getint('Logging','noise_limit')
+		self.use_sqlite = self.config.getboolean('Logging','use_sqlite')
+		self.use_screenlog = self.config.getboolean('Logging','use_screenlog')
+		self.use_twitter = self.config.getboolean('Twitter','use_twitter')
+		self.twitter_limit = self.config.getint('Twitter','noise_limit')
+		self.twitter_username = self.config.get('Twitter','username')
+		self.twitter_password = self.config.get('Twitter','password')
+		self.sms_limit = self.config.getint('SMS','noise_limit')
+		self.use_sms = self.config.getboolean('SMS','use_sms')
+		self.sms_username = self.config.get('SMS','username')
+		self.sms_password = self.config.get('SMS','password')
+		self.sms_from = self.config.get('SMS','from')
+		self.sms_to = self.config.get('SMS','to')
 
-					
+		logging.info("Torstinator %.2f" % version)
+		logging.info("Limit: %d" % self.log_limit)
+		logging.info("Twitter: %d (limit: %d)" % (self.use_twitter, self.twitter_limit))
+		logging.info("sqlite: %d" % (self.use_sqlite))
+		logging.info("sms: %d (limit: %d)" % (self.use_sms, self.sms_limit) )
+		logging.info("Log to screen: %d" % self.use_screenlog)
+
+		if twitter_enabled and self.use_twitter:
+			logging.info("Twitter initialized")
+			self.twitapi = twitter.Api(username=self.twitter_username,password=self.twitter_password)
+
+
+		if sqlite_enabled and self.use_sqlite:
+			logging.debug("Connecting to database")
+			if not os.path.isfile(database_name):
+				logging.warning("Database file not found")
+				self.con = sqlite3.connect(database_name,isolation_level=None)
+				c = self.con.cursor()
+				c.execute('''create table noise (datetime text, noise real);''')
+			else:
+				logging.debug("Database file found")
+				self.con = sqlite3.connect(database_name,isolation_level=None)
+				logging.debug("Database connected")
+
+	def alert(self, noise):
+		msg = "Noise level exceeded limits, amount %d, limit %d" 
+		if self.use_screenlog and noise > self.log_limit:
+			msg = msg % (noise, self.log_limit)
+			logging.info(msg)
+
+		if self.use_twitter and twitter_enabled and noise > self.twitter_limit and self.twitter_username != "" and self.twitter_password != "":
+			msg = msg % (noise, self.twitter_limit)
+			status = self.twitapi.PostUpdate(self.twitter_username, self.twitter_password, msg)
+			logging.info("Alerted via twitter")
+
+		if self.use_sms and noise > self.sms_limit and self.sms_username != "" and self.sms_password != "" and self.sms_to != "" and self.sms_from != "":
+			msg = msg % (noise, self.sms_limit)
+			sendSms(self.sms_username,self.sms_password,self.sms_from,self.sms_to,msg)
+			logging.info("Alerted via SMS")
+
 
 	def monitor(self):
 		logging.debug("Monitoring")
@@ -123,11 +193,11 @@ class Torstinator:
 			pass
 
 		if stream == None:
-			logging.debug("Failed to open microphone")
+			logging.critical("Failed to open microphone")
 			return
 		logging.debug("Entering listen loop")
 		while True:
-			try:	
+			try:
 				curday = strftime("%d")
 				if (self.day != curday):
 					print("Day changed, restarting")
@@ -138,12 +208,13 @@ class Torstinator:
 				level = int(audioop.max(data,2))
 				barktime=int(time.time())
 				t = (barktime, level)
-				logging.info('Noise %d' % (level))
-				self.con.execute('INSERT INTO noise VALUES (?,?);',t)
-				if (level > self.limit and self.username != None and self.password != None and self.sender != None and self.to != None):
+				if self.use_screenlog:
+					logging.info('Noise %d' % (level))
 
-					msg = "There is noise in your livingroom (level %s)" % (level)
-					# sendSms(self.username,self.password,self.sender,self.to,msg)
+				if sqlite_enabled and self.use_sqlite:
+					self.con.execute('INSERT INTO noise VALUES (?,?);',t)
+				
+				self.alert(level)
 
 			except KeyboardInterrupt:
 				print("Interrupted by ctrl-c")
@@ -152,23 +223,8 @@ class Torstinator:
 		stream.close()
 		p.terminate()
 
-parser = optparse.OptionParser(version='Torstinator %.2f' % version)
-parser.add_option('-d', '--debug', action='store_true', dest='debug', default=False, help='debug')
-parser.add_option('-l', '--limit', dest='limit', default=10000, type='int', help='limit')
-parser.add_option('-u', '--username', dest='username', default=None, type='str', help='saunalahti username')
-parser.add_option('-p', '--password', dest='password', default=None, type='str', help='saunalahti password')
-parser.add_option('-f', '--from', dest='sender', default=None, type='str', help='saunalahti sender')
-parser.add_option('-t', '--to', dest='to', default=None, type='str', help='saunalahti destination')
-(options, args) = parser.parse_args()
-	
-
 T = Torstinator()
-
 logging.debug("Setting limits")
-T.limit = options.limit
-T.username = options.username
-T.password = options.password
-T.sender = options.sender
-T.to = options.to
 logging.debug("Entering monitor mode")
+
 T.monitor()
