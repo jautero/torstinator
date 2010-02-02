f787c7e6c485f589d3c555c410bb799caa876068
tero.heino <tero.heino@a5b37900-4481-11de-b77d-cbd14124f061>
2010-02-02 08:21:13 +0000

    clear screen works with nt/posix
    git-svn-id: http://torstinator.googlecode.com/svn/trunk@22 a5b37900-4481-11de-b77d-cbd14124f061

--- TorstinatorDev.py
+++ TorstinatorDev.py
@@ -7,11 +7,19 @@ import os
 import audioop
 import wave
 
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s\t%(levelname)s\t%(message)s')
+
+
+
+
 try:
     import pyaudio 
 except ImportError:
-    print "You need to have pyaudio library ", \
-	"(http://people.csail.mit.edu/hubert/pyaudio/)"
+    logging.critical("You need to have pyaudio library "+ \
+	"(http://people.csail.mit.edu/hubert/pyaudio/)")
     sys.exit(1)
 
 
@@ -21,23 +29,24 @@ __version__ = 1.0
 SAMPLE_RATE = 44100
 BUFFER_SIZE = 30
 
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s\t%(levelname)s\t%(message)s')
-
 
 class AudioBank:
+    """ AudioBank class holds the noise level and raw audio
+        data """
+    
     audio_data = []
     noise_levels = []
-    paudio  = None
-    
+
     silence = 70
     noise = 20000
     
-    def __init__(self, paudio):
-         self.paudio = paudio
-    
+    def __init__ (self):
+        """ init """
+
+
     def push(self, newsounds, levels):
+        """ push(rawsounddata, soundlevel) pushes raw data and
+            noise levels to arrays for further processing """
         self.audio_data.append(newsounds)
         self.noise_levels.append(levels)
         if len(self.audio_data) > BUFFER_SIZE:
@@ -46,22 +55,19 @@ class AudioBank:
         self.status()
         
     def buffer_size(self):
+        """ buffer_size() returns the size of buffer in seconds """
         return len(self.audio_data)
     
     def get_data(self):
+        """ get_data() returns raw audio data for the 
+            duration of BUFFER_SIZE """
         return ''.join(self.audio_data)
     
-    
-    def save_buffer(self, filename):
-        wavefile = wave.open(filename, 'wb')
-        wavefile.setnchannels(1)
-        wavefile.setsampwidth(self.paudio.get_sample_size(pyaudio.paInt16))
-        wavefile.setframerate(SAMPLE_RATE)
-        wavefile.writeframes(self.get_data())
-        wavefile.close()
-
-    def noisetransformer(self, level):
-        percentage = (float(level) - float(self.silence))/float(self.noise)*100
+    def noiseleveltopercentage(self, level):
+        """ noiseleveltoprecentage(noiselevel) transforms
+            your noise level into percentage value """
+        percentage = (float(level) - float(self.silence))
+        percentage = (percentage / float(self.noise)) * 100
         if percentage < 0:
             return 0
         elif percentage > 100:
@@ -70,35 +76,45 @@ class AudioBank:
             return percentage
             
     def status(self):
-        os.system('clear')
-        logging.debug("| AudioBank now has %d seconds stored" % len(self.audio_data) )
-        logging.debug("|-----------------------------------")
-        logging.debug("| Max noise: %d" % max(self.noise_levels))
+        """ status() brings up a status of the data that bank 
+            currently holds """
+        if os.name == "posix":
+            os.system('clear')
+        if os.name == "nt":
+            os.system('cls')
+
+        print "| AudioBank now has %d seconds stored" % \
+                    (len(self.audio_data)) 
+        print "|-----------------------------------"
+        print "| Max noise: %d" % max(self.noise_levels)
         average = 0
         for i in self.noise_levels:
             average += i
         average = average/len(self.noise_levels)
-        logging.debug("| Average noise: %d" % average)
-        logging.debug("|-----------------------------------")
+        print "| Average noise: %d" % average
+        print "|-----------------------------------"
         for i in self.noise_levels:
             output = ""
-            for stars in range(0,int(self.noisetransformer(i))):
+            for level in range(0, int(self.noiseleveltopercentage(i))):
                 output = output+"#"
             if i == max(self.noise_levels):
                 output = output + " (MAX)"
-            logging.debug("|%s" % output)
+            print "|%s" % output
             
 class Torstinator:
     """ Class for doing all the work of Torstinator - The bark detector"""
     paudio = None
     stream = None
     audiobank = None
+    
     def __init__(self):
         """ Constructor, set the defaults, read config etc """
         try:
             logging.debug("Setting up PyAudio")
             self.paudio = pyaudio.PyAudio()
-            self.audiobank = AudioBank(self.paudio)
+
+            self.audiobank = AudioBank()
+            
             logging.debug("Setting up audiostream")
             self.stream = self.paudio.open(format = pyaudio.paInt16,
                             channels = 1,
@@ -108,7 +124,6 @@ class Torstinator:
 
         except:
             print "Unexpected error:", sys.exc_info()[0]
-            pass
 
         if self.stream == None:
             logging.critical("Failed to open microphone")
@@ -116,8 +131,19 @@ class Torstinator:
         
         self.monitor()
             
-        
+    def save_buffer(self, filename):
+        """ save_buffer(filename) saves current audiobank buffer
+            to hard disk """
+        wavefile = wave.open(filename, 'wb')
+        wavefile.setnchannels(1)
+        wavefile.setsampwidth(self.paudio.get_sample_size(pyaudio.paInt16))
+        wavefile.setframerate(SAMPLE_RATE)
+        wavefile.writeframes(self.audiobank.get_data())
+        wavefile.close()
+
     def monitor(self):
+        """ monitor() receive data from microphone
+            and act accordingly """
         while True:
             try:
                 data = self.stream.read(SAMPLE_RATE)
@@ -126,7 +152,6 @@ class Torstinator:
                 # logging.info("Noise: %d" % level)
             except IOError:
                 logging.warning("PyAudio failed to read device, skipping")
-                pass
         
             except KeyboardInterrupt:
                 # self.audiobank.save_buffer("poo.wav")
@@ -138,6 +163,11 @@ class Torstinator:
     def __del__(self):
         self.stream.close()
         self.paudio.terminate()
+
+
+if __name__ == "__main__":
+    T = Torstinator()
+
 
 """    def monitor(self):
         logging.debug("Entering listen loop")
@@ -174,7 +204,6 @@ class Torstinator:
                     wavefile.writeframes(streamdata)
                     wavefile.close()
     
-                
                 if self.use_screenlog:
                     logging.info('Noise %d' % (level))
 
@@ -192,5 +221,3 @@ class Torstinator:
         logging.debug("Closing session")
         stream.close()
         paudio.terminate()"""
-
-T = Torstinator()
